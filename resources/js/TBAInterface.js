7310da354c8f0e58dce72c656a334ac6b23e8f66
Ben Griffin <36111594+bengrif@users.noreply.github.com>
2023-03-06 16:42:23 -0700

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "6igw58AXBv815k8q2y4GXJvd1Zzx3laTbnX7tqqMQZcQtXjeEdt381ZszFgxY2aR";
 /**
  * Get list of teams in event
  *
